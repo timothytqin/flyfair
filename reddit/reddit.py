c6bf062b5164dad326f8f4c5006d6c15d3bc842f
Steve Han <stevehan2001@gmail.com>
2021-11-13 18:31:47 -0600

    server working

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 import praw
-import datetime as dt
 import pandas as pd
+from flask import Flask, request
 
-
-# In[2]:
+app = Flask(__name__)
 
 
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
-
-
-# In[3]:
 
 
 reddit = praw.Reddit(client_id='CN4F33LP3atBxFBBiLkYDg',
@@ -26,18 +19,12 @@ reddit = praw.Reddit(client_id='CN4F33LP3atBxFBBiLkYDg',
                     user_agent='stevehan2001') ## to use this, make a Reddit app. Client ID is in top left corner, client secret is given, and user agent is the username that the app is under
 
 
-# In[4]:
-
 
 sub_reddits = reddit.subreddit('wallstreetbets')
-stocks = ["GME", "AMC"] 
 # For example purposes. To use this as a live trading tool, you'd want to populate this with tickers that have been mentioned on the pertinent community (WSB in our case) in a specified period.
 
 
-# In[5]:
-
-
-def commentSentiment(ticker, urlT):
+def commentSentiment(urlT):
     subComments = []
     bodyComment = []
     try:
@@ -79,83 +66,45 @@ def commentSentiment(ticker, urlT):
     
     return(averageScore)
 
-
-# In[6]:
-
-
-def latestComment(ticker, urlT):
-    subComments = []
-    updateDates = []
-    try:
-        check = reddit.submission(url=urlT)
-        subComments = check.comments
-    except:
-        return 0
-    
-    for comment in subComments:
-        try: 
-            updateDates.append(comment.created_utc)
-        except:
-            return 0
-    
-    updateDates.sort()
-    return(updateDates[-1])
-
-
-# In[7]:
-
-
-def get_date(date):
-    return dt.datetime.fromtimestamp(date)
-
-
-# In[8]:
-
-
-submission_statistics = []
-d = {}
-for ticker in stocks:
-    for submission in reddit.subreddit('wallstreetbets').search(ticker, limit=130):
+@app.route('/')
+def getRedditSentiment():
+    print('route accessed')
+    ticker = request.args.get('ticker')
+    results = []
+    for submission in reddit.subreddit('wallstreetbets').search(ticker, limit=10):
         if submission.domain != "self.wallstreetbets":
             continue
         d = {}
-        d['ticker'] = ticker
         d['num_comments'] = submission.num_comments
-        d['comment_sentiment_average'] = commentSentiment(ticker, submission.url)
+        d['comment_sentiment_average'] = commentSentiment(submission.url)
+        print(d['comment_sentiment_average'])
         if d['comment_sentiment_average'] == 0.000000:
             continue
-        d['latest_comment_date'] = latestComment(ticker, submission.url)
-        d['score'] = submission.score
         d['upvote_ratio'] = submission.upvote_ratio
         d['date'] = submission.created_utc
-        d['domain'] = submission.domain
-        d['num_crossposts'] = submission.num_crossposts
-        d['author'] = submission.author
-        submission_statistics.append(d)
-    
-dfSentimentStocks = pd.DataFrame(submission_statistics)
-
-_timestampcreated = dfSentimentStocks["date"].apply(get_date)
-dfSentimentStocks = dfSentimentStocks.assign(timestamp = _timestampcreated)
-
-_timestampcomment = dfSentimentStocks["latest_comment_date"].apply(get_date)
-dfSentimentStocks = dfSentimentStocks.assign(commentdate = _timestampcomment)
-
-dfSentimentStocks.sort_values("latest_comment_date", axis = 0, ascending = True,inplace = True, na_position ='last') 
-
-dfSentimentStocks
+        results.append(d)
+    print(results)
+    return {'result': results}
 
 
-# In[9]:
 
 
-dfSentimentStocks.author.value_counts()
-
-
-# In[10]:
-
-
-dfSentimentStocks.to_csv('Reddit_Sentiment_Equity.csv', index=False) 
-
-
-# In[ ]:
+# submission_statistics = []
+# d = {}
+# stocks = ["GS"] 
+# for ticker in stocks:
+#     for submission in reddit.subreddit('wallstreetbets').search(ticker, limit=10):
+#         if submission.domain != "self.wallstreetbets":
+#             print(submission.domain)
+#             continue
+#         d = {}
+#         d['num_comments'] = submission.num_comments
+#         d['comment_sentiment_average'] = commentSentiment(submission.url)
+#         print(d['comment_sentiment_average'])
+#         if d['comment_sentiment_average'] == 0.000000:
+#             print('average is 0')
+#             continue
+#         d['upvote_ratio'] = submission.upvote_ratio
+#         d['date'] = submission.created_utc
+#         d['author'] = submission.author
+#         submission_statistics.append(d)
